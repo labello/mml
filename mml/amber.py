909afa9427d73b52aba6e540c8cbc08188ba6121
Nicholas Labello <nick.labello@gmail.com>
2013-07-23 08:26:54 -0500

    adding all files that should be tracked

---
+++ mml/amber.py
@@ -0,0 +1,150 @@
+
+import logging
+import math
+
+from molecules import ethane,propane
+import measure 
+
+# change default level of logger to 10 to print each parmater lookup
+logging.basicConfig(filename=__name__,level=30)
+
+def ambergaffenergy(coordinates,system=ethane.inputdata):
+
+    from amberff import gaffparams as ffparams
+     
+    Ebond    = calc_bond_energy(ffparams,system,coordinates)
+    Eangle   = calc_angle_energy(ffparams,system,coordinates) 
+    Etorsion = calc_torsion_energy(ffparams,system,coordinates)
+    EVdW,Eel = calc_nonbonded_energy(ffparams,system,coordinates)
+
+    E = Ebond + Eangle + Etorsion + EVdW + Eel
+
+    return E
+    
+def amber94energy(coordinates,system=ethane.inputdata):
+
+    from amberff import amber94params as ffparams
+     
+    Ebond    = calc_bond_energy(ffparams,system,coordinates)
+    Eangle   = calc_angle_energy(ffparams,system,coordinates) 
+    Etorsion = calc_torsion_energy(ffparams,system,coordinates)
+    EVdW,Eel = calc_nonbonded_energy(ffparams,system,coordinates)
+
+    E = Ebond + Eangle + Etorsion + EVdW + Eel
+    
+    return E
+    
+def calc_bond_energy(ffparams,system,coordinates):
+    e = 0.0
+    for bond in system.Bonds:
+        atom1,atom2 = coordinates[bond.i],coordinates[bond.j]
+        params = get_bond_params(ffparams,system,bond)
+        r0 = params.r0
+        k = params.k
+        r = measure.distance(atom1,atom2)
+        e += k * (r-r0)**2 
+    return e
+    
+def calc_angle_energy(ffparams,system,coordinates):
+    e = 0.0
+    for angle in system.Angles:
+        atom1,atom2,atom3 = coordinates[angle.i],coordinates[angle.j],coordinates[angle.k]
+        params = get_angle_params(ffparams,system,angle)   
+        k = params.k
+        theta0 = params.theta0
+        theta = measure.vec_angle(atom1,atom2,atom3)
+        e += k * (theta - theta0)**2
+    return e
+
+def calc_torsion_energy(ffparams,system,coordinates):
+    e = 0.0
+    for tor in system.Torsions:
+        atom1,atom2,atom3,atom4 = coordinates[tor.i],coordinates[tor.j],coordinates[tor.k],coordinates[tor.l]
+        params = get_tor_params(ffparams,system,tor)
+        
+        Vn2 = params.Vn2
+        gamma = params.gamma
+        period = params.period
+        bondpaths = params.bondpaths
+        phi = measure.vec_dihedral(atom1,atom2,atom3,atom4)
+        
+        e += Vn2 * (1 + math.cos(period*phi - gamma))
+    return e
+    
+def calc_nonbonded_energy(ffparams,system,coordinates):
+    NAtoms = len(system.Atoms)
+    evdw = 0
+    eel = 0
+    for i in xrange(0,NAtoms):
+        for j in xrange(0,i):
+            ai,aj = system.Atoms[i],system.Atoms[j]
+            if j not in (ai.fnn + ai.snn):
+                iparams = get_vdw_params(ffparams,ai)
+                jparams = get_vdw_params(ffparams,aj)
+                Rstarij = iparams.R + jparams.R
+                epsilonij = math.sqrt(iparams.epsilon * jparams.epsilon)
+                Aij = epsilonij * (Rstarij)**12
+                Bij = 2 * epsilonij * (Rstarij)**6
+                Rij = measure.distance(coordinates[i],coordinates[j])
+                vdw = (Aij / (Rij)**12) - (Bij / (Rij)**6)
+                el  = (ai.charge * aj.charge) / (epsilonij * Rij)
+
+# apply scale factor for atoms separated by 3 bonds
+                if j in ai.tnn:
+                    vdw = vdw * (1.0/2.0) 
+                    el  =  el * (1.0/1.2)
+
+                evdw += vdw
+                eel += el
+    return evdw,eel
+               
+
+def get_bond_params(ffparams,system,bond):
+    a1 = system.Atoms[bond.i]
+    a2 = system.Atoms[bond.j]
+    dks = ['-'.join([a1.type,a2.type]),
+           '-'.join([a2.type,a1.type])]
+    params = None
+    for dk in dks:
+        try:
+            params = ffparams.Bonds[dk]
+            logging.debug('Using parameter for %s' % dk)
+            break
+        except KeyError:
+            logging.critical('Parameter %s not found' % dk)           
+    return params
+    
+def get_angle_params(ffparams,system,angle):
+    try:        
+        dk = angle.label
+        params = ffparams.Angles[dk]
+    except KeyError:
+        dk = angle.label ; dk.reverse()
+        params = ffparams.Angles[dk]
+     
+    return params
+     
+def get_tor_params(ffparams,system,torsion):
+    a1 = system.Atoms[torsion.i]
+    a2 = system.Atoms[torsion.j]
+    a3 = system.Atoms[torsion.k]
+    a4 = system.Atoms[torsion.l] 
+    
+    dks = ['-'.join([ a1.type,a2.type,a3.type,a4.type]),
+    '-'.join([ a4.type,a3.type,a2.type,a1.type]),
+    '-'.join([ 'X',a3.type,a2.type,'X']),
+    '-'.join([ 'X',a2.type,a3.type,'X']) ]
+        
+    for dk in dks:
+        try:
+            params = ffparams.Torsions[dk]
+            logging.debug('Using parameter for %s' % dk)
+            break
+        except KeyError:
+            logging.critical('Parameter %s not found' % dk)
+    return params
+
+def get_vdw_params(ffparams,atom):
+    dk = atom.type
+    params = ffparams.VdWs[dk]
+    return params
---
+++ mml/main.py
@@ -0,0 +1,59 @@
+
+import argparse
+import logging
+import sys
+from collections import namedtuple
+import math
+import numpy as np
+import scipy.optimize
+
+
+import amber
+import measure 
+
+# define molecular system input
+import molecules
+system = molecules.benzene.inputdata
+
+def main():
+  
+    coordinates = input2cartesian_array(system.Atoms)  
+    print_cartesian_output(system.Atoms,coordinates)    
+    print amber.ambergaffenergy(coordinates,system)
+
+    flat_coords = np.ndarray.flatten(coordinates)
+
+    res = scipy.optimize.minimize(wrap_energy_for_minimizer,flat_coords,args=(system,),
+     method='CG',tol=5.0,bounds=None)
+     
+    print res
+
+def wrap_energy_for_minimizer(flat_coords,system):
+    c = flat_coords.reshape(-1,3)
+    print_cartesian_output(system.Atoms,c)
+    e = amber.ambergaffenergy(c,system)
+#    print e
+    return e
+    
+def print_cartesian_output(Atoms,coordinates):
+    print len(coordinates)
+    print 
+    for i in range(0,len(Atoms)):
+        # replace w/ log
+        print "{0:4s} {1:12.5f} {2:12.5f} {3:12.5f}".format(
+         Atoms[i].type, coordinates[i][0],coordinates[i][1],coordinates[i][2])
+    
+def input2cartesian_array(Atoms):
+    return np.vstack(atom.vec for atom in Atoms )
+    
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='\n\n    Molecular Modeling Lite\n',add_help=True)
+    parser.add_argument('--molecule',help='The molecular system from ../mml/molecules')
+    parser.add_argument('--outputlevel',default=30,help='''The priority of the 
+     output that is generated.  Lower numbers correspond to lower priority 
+     messages being printed.  Meaningful values are 10 (most output) , 20, 30,
+     40, or 50 (least output)''')
+    args = parser.parse_args()
+
+    main()
