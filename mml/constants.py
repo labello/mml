db84cb0e2fff8cec1ae521d4fe86356ae7be097f
Nicholas Labello <nick.labello@gmail.com>
2013-09-24 10:31:07 -0500

    Create constants module and populate with a few conversions

---
+++ mml/constants.py
@@ -0,0 +1,17 @@
+
+
+##############################################################################
+#                     physical constants
+
+k = 1.3806488 * 10**-23   # Boltzman's constants kg * m**2
+                          #                      ---------
+                          #                       K * s**2
+
+##############################################################################
+
+SECONDS_TO_FEMPTOSECONDS = 10**15
+SECONDS_TO_PICOSECOND    = 10**12
+FEMPTOSECONDS_TO_SECONDS = 1.0 / SECONDS_TO_FEMPTOSECONDS
+PICOSECONDS_TO_SECONDS   = 1.0 / SECONDS_TO_PICOSECONDS
+AMU_TO_KG                = 1.66053892 * (10**-27)
+ANGSTROM_TO_METER        = 10**-10
